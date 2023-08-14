10e291dd7e01dcd3ad240758b4d2334838390ca8
unknown <ruslanchik_nadyrov@mail.ru>
2023-08-14 16:48:30 +0600

    refres add-groups

--- client/src/components/Add-groups.vue
+++ client/src/components/Add-groups.vue
@@ -1,27 +1,46 @@
 <script setup lang="ts">
-import { directors } from "@/API/api-enterprises-institutions";
+import { directors, groups } from "@/API/api-enterprises-institutions";
+import router from "@/router";
 import axios from "axios";
 import { ref } from "vue"
 
 const group = ref<string>("")
 const course = ref<string>("")
 
-async function getDirectorsId() {
+async function getInstitutionId() {
 	const data = (await axios.get(directors)).data
 	const director = data.find(item => item.id === Number(localStorage.getItem("id")))
 	if (director.institution) {
-		console.log(director.institution.id);
+		localStorage.setItem("institutionId", director.institution.id)
+	}
+}
+getInstitutionId()
+
+const instance = axios.create({
+	baseURL : groups
+});
+instance.defaults.headers.common['Authorization'] = localStorage.getItem("token");
+
+async function addGroup () {
+	try {
+		await instance.post(groups, {
+			groupName: group.value,
+			course: course.value,
+			institutionId: Number(localStorage.getItem('institutionId'))
+		})
+		router.push("/groups")
+	} catch (error) {
+		console.log(error);
 	}
 }
 
-getDirectorsId()
 </script>
 
 <template>
 	<main>
 		<div class="add-enterprises-institutions">
 			<h1>Новая группа</h1>
-			<form class="add-enterprises-institutions__form">
+			<form class="add-enterprises-institutions__form" @submit.prevent="addGroup()">
 				<input type="text" placeholder="Группа" v-model="group">
 				<input type="text" placeholder="Курс" v-model="course">
 				<button type="submit">Создать</button>
